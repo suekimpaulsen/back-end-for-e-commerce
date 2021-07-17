1e6eff9be68c6da2975844b3cdbc161275c2be9a
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-07-16 23:19:32 -0500

    category.findall added & tested on insomnia

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,8 +5,30 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  Category.findAll({})
-  // be sure to include its associated Products
+  Category.findAll({
+    attributes: [
+      'id',
+      'category_name'
+    ],
+    // be sure to include its associated Products
+    include: [
+      {
+        model: Product,
+        attributes: [
+          'id',
+          'product_name',
+          'price',
+          'stock',
+          'category_id'
+        ]
+      }
+    ]
+  }).then(dbCategoryData => {
+    res.json(dbCategoryData)
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
